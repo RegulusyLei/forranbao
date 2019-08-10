ecb50d95694673a0082a2a8720538e891a9def6d
张磊 <zhanglei@everjiankang.com>
2019-08-10 18:16:31 +0800

    g

---
+++ code/test1.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data () {
+    return {
+      attr1: '',
+      attr2: ''
+    }
+  },
+  methods: {
+    test1 () {
+      this.$emit('event', {
+        attr1: this.attr1,
+        attr2: this.attr2
+      })
+    },
+    test2 () {
+      this.$emit('event', {
+        attr1: this.attr1,
+        attr2: this.attr2
+      })
+    }
+  }
+}
+</script>
